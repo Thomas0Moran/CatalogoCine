89965cc68b338d12acb221bc7ed2e1b0b76ed95b
Thomas-Moran <thomas.l.moran.ct@gmail.com>
2021-09-14 17:42:05 -0300

    se agrego un proyecto y dentro de el se agrego un archivo Model

---
+++ Entity FrameworkDatabaseFirst/02EntityFrameworkDatabaseFirst/02EntityFrameworkDatabaseFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02EntityFrameworkDatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NorthwindEntities entities = new NorthwindEntities();
+
+            /*
+            Customer costumer = new Customer()
+            {
+                CustomerID = "EDUIT",
+                CompanyName = "EducacionIT",
+                Address = "Lavalle",
+                City = "CABA",
+                ContactName = "contacto@gmail.com",
+                ContactTitle = "Mail de contacto",
+                Country = "AR",
+                Phone = "08001110000",
+                PostalCode = "1001",
+                Region = "CABA",
+            };
+            entities.Customers.Add(costumer);
+            entities.SaveChanges();
+            */
+            
+            
+            var lista = entities.Customers.ToList();
+            foreach(var cliente in lista)
+            {
+                Console.WriteLine(cliente.CompanyName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
